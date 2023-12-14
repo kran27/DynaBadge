3bbbe35d495aead58a5dd675caabc77166f3e3a4
Kran <56488228+kran27@users.noreply.github.com>
2023-12-14 00:26:05 -0800

    Update discord-badge.js
    un-hardcode guildid
    fixes github not using the image

--- api/discord-badge.js
+++ api/discord-badge.js
@@ -1,8 +1,17 @@
 // vercel serverless function
 module.exports = async (req, res) => {
   const https = require('https');
+  const url = require('url');
 
-  https.get('https://discordapp.com/api/guilds/470671750087180289/widget.json', (apiRes) => {
+  const queryObject = url.parse(req.url,true).query;
+  const guildId = queryObject.guild;
+
+  if (!guildId) {
+    res.status(400).send('Error: Missing guild query parameter');
+    return;
+  }
+
+  https.get(`https://discordapp.com/api/guilds/${guildId}/widget.json`, (apiRes) => {
     let data = '';
 
     apiRes.on('data', (chunk) => {
@@ -13,10 +22,10 @@ module.exports = async (req, res) => {
       const presenceCount = JSON.parse(data).presence_count;
       const badgeUrl = `https://img.shields.io/static/v1?style=flat-square&label=%20&message=${presenceCount}%20Online&color=5662f6&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAABkUlEQVRIx+2VLUgEURSFvzdF1KDIgEUZsK0YhNXoDwgqaBQ0GATBNZgEk3mzxWI2GVw2iKhBcFabYNLsCuIPazDsYjuGnX3Mm3GFXatnyr3nnXfnvTuHO/AP46bqYp4JJqmaycRKSDchJc5MrUkBTbDFIl1RukuFETqAL+7xyUd8jRP2TSl1FO2oFWwntw+q2lKBqgK3wKFaxWF8e0btIPOX99szGFA/r22aYMA8e8B62y5aq1/gTu3iDlCQIN+1BMoodNhQGdCSygl1gFYT1FzD1Hqw3IMif2o2oV71GHduVTbnkcdrHFn2uOF/c0HZ0Wc9Rh2iEos/bfTRRAGjHr1uRfk2nrLRjHWMT9bR95HqbFi/rzYcdiPqy1VSbqTU161wyjBjCfaWexbwUwPlhwItwfvrSPN4c/JLir/qi1w6+ZvHMo8xYogiATkKPDnCFwpsElBgKMY+slLvbd6ZR2H0yTqUjZ7OxmB1ZlK+4U9AvvZskelm59eM3bwnP73co5yudfBbC3SgG+XU8/8/jOMb/1FYQfrpXm4AAAAASUVORK5CYII=`;
 
-      res.writeHead(302, {
-        'Location': badgeUrl
+      https.get(badgeUrl, (badgeRes) => {
+        res.setHeader('Content-Type', 'image/svg+xml');
+        badgeRes.pipe(res);
       });
-      res.end();
     });
   }).on('error', (err) => {
     res.status(500).send(`Error: ${err.message}`);
